d5931c44202fd2cfaa1c6a460fcc82d2f266c74a
andrehebra <52555960+andrehebra@users.noreply.github.com>
2024-02-16 22:18:01 -0500

    example is working now thank god

--- hw2/lex.c
+++ hw2/lex.c
@@ -12,6 +12,11 @@ char reserved[14][10] = {"const", "var", "procedure", "call", "begin", "end", "i
 //define the special symbols
 char special[13][1] = {"+", "-", "*", "/", "(", ")", "=", "," , ".", "<", ">", ";" , ":"};
 
+typedef struct ident {
+    char name[12];
+    char value[6];
+} ident;
+
 typedef enum {
     skipsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5,
     multsym = 6, slashsym = 7, fisym = 8, eqlsym = 9, neqsym = 10, lessym = 11, leqsym = 12, 
@@ -22,11 +27,41 @@ typedef enum {
     readsym = 32, elsesym = 33
 } token_type;
 
+char* compareIdents(char* bigList, ident* wordList, int wordLength) {
+    for (int i = 0; i < wordLength; i++) {
+        int requiredCount = strlen(wordList[i].name);
+        int count = 0;
+        for (int j = 0; j < strlen(wordList[i].name); j++) {
+            if (wordList[i].name[j] == bigList[j]) {
+                count++;
+            }
+        }
+
+        if (count == requiredCount) {
+            return wordList[i].name;
+        }
+
+        count = 0;
+    }
+
+    return "NEG";
+}
+
+char* getDigits(char* word, char* returnWord) {
+    int position = 0;
+    while (word[position] == '0' ||word[position] == '1' ||word[position] == '2' ||word[position] == '3' ||word[position] == '4' ||word[position] == '5' ||word[position] == '6' ||word[position] == '7' ||word[position] == '8' ||word[position] == '9') {
+        strcat(returnWord, (char[2]) {word[position], '\0'});
+        position++;
+    }
+
+    return returnWord;
+}
+
 // removes whitespace, tabs, and new line characters from the string passed to it utilizing recursion
 char* removeWhiteSpace(char* line) {
     int currentLineSize = strlen(line);
     for (int j = 0; j < currentLineSize; j++) {
-        if (line[j] == ' ' || line[j] == '\t' || line[j] == '\n' || line[j] == ';') {
+        if (line[j] == ' ' || line[j] == '\t' || line[j] == '\n') {
             for (int k = j; k < currentLineSize; k++) {
                 line[k] = line[k+1];
             }
@@ -51,8 +86,10 @@ char* removeWord(char* line, char* word) {
             for (int k = i; k < strlen(line); k++) {
                 line[k] = line[k+wordLength];
             }
+            
          }
          count = 0;
+         return line;
     }
 
     return line;
@@ -72,6 +109,21 @@ char* findReserved(char* line) {
             case 'v':
                 if (line[i+1] == 'a' && line[i+2] == 'r') {
                     return "var";
+                }
+                break;
+            case 'b':
+                if (line[i+1] == 'e' && line[i+2] == 'g' && line[i+3] == 'i' && line[i+4] == 'n') {
+                    return "begin";
+                }
+                break;
+            case ':':
+                if (line[i+1] == '=') {
+                    return ":=";
+                }
+                break;
+            case 'e':
+                if (line[i+1] == 'n' && line[i+2] == 'd') {
+                    return "end";
                 }
                 break;
         }
@@ -103,6 +155,10 @@ int main(int argc, char *argv[]) {
     //create a token list to be printed later
     char tokenList[1000][200];
     int tokenPosition = 0;
+
+    //create a list of variables
+    ident identList[1000];
+    int identPosition = 0;
 
 
     printf("\n\nLexeme Table:\n\nlexeme token type\n");
@@ -142,18 +198,115 @@ int main(int argc, char *argv[]) {
         strcpy(lineByLine[i], removeWhiteSpace(lineByLine[i]));
     }
 
+    
+    
+    
+
     for (int i = 0; i < lineCount; i++) {
         char reservedWord[20];
 
         strcpy(reservedWord, findReserved(lineByLine[i]));
-
+        
+        // var
         if (strcmp(reservedWord, "var") == 0) {
+            int printSemi = 0;
             strcpy(lineByLine[i], removeWord(lineByLine[i], reservedWord));
-            printf("%-10s 29", reservedWord);
+            printf("\n%-10s 29", reservedWord);
             strcpy(tokenList[tokenPosition], "29");
             tokenPosition++;
+            for (int j = 0; j < strlen(lineByLine[i]); j++) {
+                if (lineByLine[i][j] == ';') {
+                    printSemi = 1;
+                } else if (lineByLine[i][j] == ',') {
+                    strcpy(tokenList[tokenPosition], "2");
+                    tokenPosition++;
+                    strcpy(tokenList[tokenPosition], identList[identPosition].name);
+                    tokenPosition++;
+                    strcpy(tokenList[tokenPosition], "17");
+                    tokenPosition++;
+
+                    printf("\n%-10s 2", identList[identPosition].name);
+                    printf("\n%-10s 17", ",");
+
+                    identPosition++;
+                } else {
+                    strcat(identList[identPosition].name, (char[2]) { lineByLine[i][j], '\0' });
+                }
+            }
+
+            strcpy(tokenList[tokenPosition], "2");
+            tokenPosition++;
+            strcpy(tokenList[tokenPosition], identList[identPosition].name);
+            tokenPosition++;
+
+            printf("\n%-10s 2", identList[identPosition].name);
+
+            identPosition++;
+
+            if (printSemi == 1) {
+                printf("\n%-10s 18", ";");
+                strcat(tokenList[tokenPosition], "18");
+                tokenPosition++;
+            }
+        }
+        
+        //begin
+        if (strcmp(reservedWord, "begin") == 0) {
+            printf("\n%-10s 21", reservedWord);
+            strcpy(tokenList[tokenPosition++],"21");
         }
 
+        // := 
+        if (strcmp(reservedWord, ":=") == 0) {
+            while (strlen(lineByLine[i]) > 0) {
+                char wordToRemove[200];
+                strcpy(wordToRemove, compareIdents(lineByLine[i], identList, identPosition));
+                if (strcmp(wordToRemove, "NEG") != 0) {
+                    removeWord(lineByLine[i], wordToRemove);
+                    printf("\n%-10s 2", wordToRemove);
+                    strcpy(tokenList[tokenPosition++], "2");
+                    strcpy(tokenList[tokenPosition++], wordToRemove);
+                } else {
+                    if (lineByLine[i][0] == ':' && lineByLine[i][1] == '=') {
+                        removeWord(lineByLine[i], ":=");
+                        strcpy(tokenList[tokenPosition++], "20");
+                        printf("\n%-10s 20", ":=");
+                    } else if (lineByLine[i][0] == '+') {
+                        removeWord(lineByLine[i], (char[2]) { '+', '\0' });
+                        strcpy(tokenList[tokenPosition++], "4");
+                        printf("\n%-10s 4", "+\0");
+                    } else if (lineByLine[i][0] == ';') {
+                        removeWord(lineByLine[i], (char[2]) { ';', '\0' });
+                        strcpy(tokenList[tokenPosition++], "18");
+                        printf("\n%-10s 18", ";\0");
+                    } else if (lineByLine[i][0] == '0' ||lineByLine[i][0] == '1' ||lineByLine[i][0] == '2' ||lineByLine[i][0] == '3' ||lineByLine[i][0] == '4' ||lineByLine[i][0] == '5' ||lineByLine[i][0] == '6' ||lineByLine[i][0] == '7' ||lineByLine[i][0] == '8' ||lineByLine[i][0] == '9') {
+                        char digits[100];
+                        digits[0] = '\0';
+                        strcpy(digits, getDigits(lineByLine[i], digits));
+                        removeWord(lineByLine[i], digits);
+                        printf("\n%-10s 3", digits);
+                        strcpy(tokenList[tokenPosition++], "3");
+                        strcpy(tokenList[tokenPosition++], digits);
+                    }
+                }
+            }
+        }
+
+        if (strcmp(reservedWord, "end") == 0) {
+            int endofprogram = 0;
+            while (strlen(lineByLine[i]) > 0 || endofprogram != 1) {
+                if (lineByLine[i][0] == '.') {
+                    removeWord(lineByLine[i], ".\0");
+                    strcpy(tokenList[tokenPosition++], "19");
+                    printf("\n%-10s 19", ".\0");
+                    endofprogram = 1;
+                } else if (lineByLine[i][0] == 'e' && lineByLine[i][1] == 'n' && lineByLine[i][2] == 'd') {
+                    removeWord(lineByLine[i], "end");
+                    strcpy(tokenList[tokenPosition++], "22");
+                    printf("\n%-10s 22", ";\0");
+                }
+            }
+        }
         //printf("%s", reservedWord);
     }
 
